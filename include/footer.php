f7d2d9db75458689e1dd138d53b956bcd44efdb2
Saroj Chaulagain <142041373+SarojChaulagain0@users.noreply.github.com>
2024-02-13 00:19:55 +1100

    Add files via upload

---
+++ include/footer.php
@@ -0,0 +1,6 @@
+<footer>
+    &copy; 2024 Ecom Company. All rights reserved.
+</footer>
+
+</body>
+</html>
---
+++ include/header.php
@@ -0,0 +1,24 @@
+<?php session_start();?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ecom</title>
+    <link rel="stylesheet" href="css/style.css">
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <style>
+        
+    </style>
+</head>
+<body>
+
+    <header>
+        <h1>Quantic Dream</h1>
+    </header>
+
+    <nav>    
+        <ul>
+            <li><a href="index.php">Home</a></li>
+        </ul>
+    </nav>
